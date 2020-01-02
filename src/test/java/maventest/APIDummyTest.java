e34c006d7c96c85a22529d0b14a6587f680a8d3b
pratibha <pratibha_agrawal@in.ibm.com>
2020-01-02 12:27:34 +0530

    PA committed

---
+++ src/test/java/maventest/APIDummyTest.java
@@ -0,0 +1,19 @@
+package maventest;
+
+import org.testng.annotations.Test;
+
+public class APIDummyTest {
+	
+	@Test
+	public void three()
+	{
+		System.out.println("three");
+	}
+	
+	@Test
+	public void four()
+	{
+		System.out.println("four");
+	}
+
+}
---
+++ src/test/java/maventest/SeleniumDummyTest.java
@@ -0,0 +1,19 @@
+package maventest;
+
+import org.testng.annotations.Test;
+
+public class SeleniumDummyTest {
+	
+	@Test
+	public void one()
+	{
+		System.out.println("one");
+	}
+	
+	@Test
+	public void two()
+	{
+		System.out.println("two");
+	}
+
+}
